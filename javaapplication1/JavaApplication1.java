942b353f34927358df0a7b032da284b690a5676e
yperezmartinez <yperezmartinez@eniac12.danielcastelao.org>
2014-10-02 09:48:14 +0200

    Mejorando en git

--- javaapplication1/JavaApplication1.java
+++ javaapplication1/JavaApplication1.java
@@ -16,7 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // Puto amo usando git
+        // Mejorando en git
     }
     
 }
